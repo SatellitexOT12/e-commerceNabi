b33d643d99a1ab9d7fc9ebfb617bd201825f9713
SatellitexOT12 <SatellitexOT12@users.noreply.github.com>
2026-04-23 01:20:30 +0000

    deploy: 3545ad5d3b4fb651b4a6a8c469d25f7c71d98e9a

---
+++ assets/settings-KRU2vhxW.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-tMXdHHYd.js";const e=async()=>{const{data:t,error:s}=await a.from("settings").select("whatsapp_numero1, whatsapp_numero2").single();if(s)throw s;return t};export{e as getWhatsAppNumbers};
